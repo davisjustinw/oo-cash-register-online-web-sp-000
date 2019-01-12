8fc9d7d6b8f0929898e8abeec997022bfa2c4357
Justin Davis <davisjustinw@gmail.com>
2019-01-12 03:58:49 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,7 +9,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    #@items << title
+    @items << title
     #@last_item = price * quantity
     @total += price * quantity
   end
