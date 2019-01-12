4a4267178c7bb75ea4b8f7773f628217bf93badc
Justin Davis <davisjustinw@gmail.com>
2019-01-12 04:01:09 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   end
   
   def add_item(title, price, quantity = 1)
-    binding.pry
+    #binding.pry
     @items << title
     #@last_item = price * quantity
     @total += price * quantity
